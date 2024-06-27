85c7ff780dbb0871b5f122b97a9f2aab552fce3b
ducthuanle <thuanvd.le@gmail.com>
2024-06-27 14:43:54 +0200

    support https

--- src/main/java/com/th/mux/config/WebConfig.java
+++ src/main/java/com/th/mux/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods(HttpMethod.POST.name(), HttpMethod.PUT.name(),
                         HttpMethod.GET.name(), HttpMethod.DELETE.name());
     }
